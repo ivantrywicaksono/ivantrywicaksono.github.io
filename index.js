5763323aacd310a19fc4d7cbde34c5cf8b6bd018
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2025-01-08 20:30:40 +0700

    lint

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const body = document.body;
-const content = document.querySelector('#content');
-const menuBtn = document.querySelector('#nav-btn');
-const navSections = document.querySelector('#nav-sections');
+const content = document.querySelector("#content");
+const menuBtn = document.querySelector("#nav-btn");
+const navSections = document.querySelector("#nav-sections");
 
 function toggleHidden(element) {
-  element.classList.toggle('show');
-};
+  element.classList.toggle("show");
+}
 
 menuBtn.addEventListener("click", () => {
   toggleHidden(navSections);
-  if (navSections.classList.value === 'show') content.classList.add('blur');
-  else content.classList.remove('blur');
+  if (navSections.classList.value === "show") content.classList.add("blur");
+  else content.classList.remove("blur");
 });
+
+const downloadCV = document.querySelector('#dwnld-cv');
+
+downloadCV.addEventListener('click', () => { browser.downloads.download({ url: './sitemap.txt' }) });
